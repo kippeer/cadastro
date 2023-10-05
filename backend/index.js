45eb52620336d050026fc9e50ace65b6f1ef88f7
Fabio Aragao Tessaro <fabiot92@gmail.com>
2023-10-05 19:05:19 -0300

    att
    att

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+ //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(3001, () => {
+    console.log('JSON Server is running')
+})
