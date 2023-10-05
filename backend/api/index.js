a38e17e8d9e8aad3d69b5d328267beab280f954a
Fabio Aragao Tessaro <fabiot92@gmail.com>
2023-10-05 19:15:14 -0300

    att
    att

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
